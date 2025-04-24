8dc0ff3e54313ce32964d850fa369902555051e0
unknown <xmlejnek@ad.fi.muni.cz>
2025-04-24 10:46:28 +0200

    rmeoved whitespaces in code

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -2,7 +2,6 @@ import sys
 import os
 import yaml
 import flask
-import
 
 app = flask.Flask(__name__)
 
@@ -40,11 +39,13 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -52,7 +53,7 @@ if __name__ == '__main__':
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice  = input("Select vulnerability: ")
+    choice = input("Select vulnerability: ")
     if choice == "1":
         new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
@@ -65,4 +66,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
